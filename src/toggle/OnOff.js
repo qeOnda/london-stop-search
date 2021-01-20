356663a4adb3fd0d4cae3251218d8d083e91022f
Shinsuke  May <s.seanmay@gmail.com>
2021-01-20 22:34:37 +0900

    add loader

--- src/toggle/OnOff.js
+++ src/toggle/OnOff.js
@@ -2,45 +2,83 @@ import React, { useState } from 'react';
 import Switch from "react-switch";
 
 
+
 function OnOff(props) {
 	const map = props.map
 	const [on, setOn] = useState(false)
+	const [spinner, setSpinner] = useState(false)
 
 	const changeState = () => {
 		if (!on)  {
-			map.setLayoutProperty('circles', 'visibility', 'visible')	
+			setSpinner(true)
+			map.addLayer({
+				'id': 'circles',
+				'type': 'circle',
+				'source': 'points',
+				'buffer': 0,
+				'paint': {
+					'circle-radius': [
+						'interpolate',
+						['linear'],
+						['zoom'],
+						9.5, 0.9,
+						15, 4,
+		            ],
+					'circle-color': 'red'
+				},
+				layout: {
+					'visibility': 'visible'
+				},
+			});
+			map.on('render', stopSpinner);
 		} else {					
-			map.setLayoutProperty('circles', 'visibility', 'none')
+			map.removeLayer('circles');
 		}
 		setOn(!on)
 	} 
 
-	return (
-		<div>
-			<label>
-	        	<span>Points ON/OFF</span>
-		        <Switch 
-		        	onChange={() => changeState()}			          
-		        	checked={on}
-		        	height={14}
-		        	width={28}
-		        	handleDiameter={12}
-		        />
-	      </label>	
-		</div>	
-	)
+	function stopSpinner (e) {
+		if (e.target && e.target.loaded()) {
+			setSpinner(false);
+			map.off('render', stopSpinner)
+		}
+ 	}
+
+	if(spinner){
+		return (
+			<div>
+				<div className='flex-parent flex-parent--center-cross flex-parent--center-main absolute top right bottom left bg-darken10 z5'>
+	        		<div className='flex-child loading'></div>
+	    		</div>
+				<label>
+		        	<span>Points ON/OFF</span>
+			        <Switch 
+			        	onChange={() => changeState()}			          
+			        	checked={on}
+			        	height={14}
+			        	width={28}
+			        	handleDiameter={12}
+			        />
+		      </label>	
+			</div>	
+		)
+	} else {
+		return (
+			<div>
+				<label>
+		        	<span>Points ON/OFF</span>
+			        <Switch 
+			        	onChange={() => changeState()}			          
+			        	checked={on}
+			        	height={14}
+			        	width={28}
+			        	handleDiameter={12}
+			        />
+		      </label>	
+			</div>	
+		)
+	}
 }
 
 export default OnOff;
 
-
-
-		// <div>
-		// 	<p>DOTS ON/OFF</p>
-		// 	<input
-		// 		onChange={() => changeState()}			          
-		// 		name="toggle"
-		// 		type="checkbox"	
-		// 		checked={on}
-		// 	/>
-		// </div>	
