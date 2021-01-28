a86888ca368a10294a708bb5fbfe8c27f7d866e4
Shinsuke  May <s.seanmay@gmail.com>
2021-01-28 13:44:23 +0900

    favicon test

--- src/text-files/data-about.js
+++ src/text-files/data-about.js
@@ -2,7 +2,6 @@ export const data = (
 	<div>
 		<p>
 			<strong>DATA</strong> used for this project was downloaded <a href="https://data.police.uk/data/" target="_blank"><u>here</u></a> from data.police.uk. 
-			
 		</p>	
 	</div>
 )
